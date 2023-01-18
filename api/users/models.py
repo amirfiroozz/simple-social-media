edea20213e10c3b08243550dd7177b1976d62a94
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 11:26:06 +0330

    users model added

--- api/users/models.py
+++ api/users/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+class User(models.Model):
+    _id = models.ObjectIdField()
+    username = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "users"
+        
+
+    
+
