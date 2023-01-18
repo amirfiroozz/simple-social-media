93b9982752dd7aba870cfaa3644611fb71496899
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 12:28:51 +0330

    comments and likes read view added

--- api/posts/views.py
+++ api/posts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from main.utils import send
+from comments.services import CommentService
+from likes.services import LikeService
 
-# Create your views here.
+commentService = CommentService()
+likeService = LikeService()
+
+
+@api_view(["GET"])
+def postCommentsListView(request , postId):
+    return send(showPostComments(postId), 200)
+
+@api_view(["GET"])
+def postLikesListView(request , postId):
+    return send(showPostLikes(postId), 200)
+
+
+def showPostComments(postId):
+    return commentService.toListOfDict(commentService.getAllCommentsByPostId(postId))
+
+def showPostLikes(postId):
+    return likeService.toListOfDict(likeService.getAllLikesByPostId(postId))
+
+
+
+
+
