abd7b1d426961c2da8b8f8bf2b81551703e9aac9
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 11:42:18 +0330

    users create/read views added

--- api/users/views.py
+++ api/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from main.utils import send , parseBody
+from .services import UserServices
 
-# Create your views here.
+userService = UserServices()
+
+
+@api_view(["GET","POST"])
+def userCreateListView(request):
+    if request.method=="GET":
+        return send(showAllusers() , 200)
+    else:
+        userData = parseBody(request.body)
+        return send(newUser(userData['username']) , 200)
+        
+@api_view(["GET"])
+def userView(request , id):
+    return send(showUser(id) , 200)
+
+
+
+def showAllusers():
+    return userService.toListOfDict(userService.getAllUsers())
+
+def newUser(username):
+    return userService.toDict(userService.newUser(username))
+
+def showUser(id):
+    return userService.toDict(userService.getUser(id))
+
