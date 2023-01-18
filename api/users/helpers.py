e345315b56d4eaeba80f291a3c2f1aec0dd55c0e
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 13:28:42 +0330

    views and method controllers seperated

---
+++ api/users/helpers.py
@@ -0,0 +1,56 @@
+from main.utils import send , sendError
+from main.errors import Error
+from .services import UserServices
+from posts.services import PostService
+from likes.services import LikeService
+from comments.services import CommentService
+
+userService = UserServices()
+postService = PostService()
+commentService = CommentService()
+likeService = LikeService()
+
+def showAllusers():
+    return send(userService.toListOfDict(userService.getAllUsers()))
+
+def newUser(username):
+    return send(userService.toDict(userService.newUser(username)))
+
+def showUser(userId):
+    user = getUser(userId)
+    if user==None:
+        return sendError(Error.userNotFoundedError())
+    return send(userService.toDict(user))
+
+def showUserPosts(userId):
+    return send(postService.toListOfDict(postService.getAllPostByUserId(userId)))
+
+def newUserPost(content , userId):
+    user = getUser(userId)
+    if user==None:
+        return sendError(Error.userNotFoundedError())
+    return send(postService.toDict(postService.newPost(content , user)))
+
+def newUserComment(content ,userId , postId ):
+    user = getUser(userId)
+    if user==None:
+        return sendError(Error.userNotFoundedError())
+    post = getPost(postId)
+    if post==None:
+        return sendError(Error.postNotFoundedError())
+    return send (commentService.toDict(commentService.newComment(content , post , user)))
+
+def newUserLike(userId , postId ):
+    user = getUser(userId)
+    if user==None:
+        return sendError(Error.userNotFoundedError())
+    post = getPost(postId)
+    if post==None:
+        return sendError(Error.postNotFoundedError())
+    return send(likeService.toDict(likeService.newLike(post , user)))
+
+def getPost(postId):
+    return postService.getPost(postId)
+
+def getUser(userId):
+    return userService.getUser(userId)
--- api/users/views.py
+++ api/users/views.py
@@ -1,16 +1,6 @@
 from rest_framework.decorators import api_view
 from main.utils import send , sendError, parseBody
-from main.errors import Error
-from .services import UserServices
-from posts.services import PostService
-from likes.services import LikeService
-from comments.services import CommentService
-
-
-userService = UserServices()
-postService = PostService()
-commentService = CommentService()
-likeService = LikeService()
+from .helpers import *
 
 
 @api_view(["GET","POST"])
@@ -44,50 +34,7 @@ def userLikeCreateView(request , userId , postId):
     return newUserLike(userId , postId)
 
 
-def showAllusers():
-    return send(userService.toListOfDict(userService.getAllUsers()))
 
-def newUser(username):
-    return send(userService.toDict(userService.newUser(username)))
-
-def showUser(userId):
-    user = getUser(userId)
-    if user==None:
-        return sendError(Error.userNotFoundedError())
-    return send(userService.toDict(user))
-
-def showUserPosts(userId):
-    return send(postService.toListOfDict(postService.getAllPostByUserId(userId)))
-
-def newUserPost(content , userId):
-    user = getUser(userId)
-    if user==None:
-        return sendError(Error.userNotFoundedError())
-    return send(postService.toDict(postService.newPost(content , user)))
-
-def newUserComment(content ,userId , postId ):
-    user = getUser(userId)
-    if user==None:
-        return sendError(Error.userNotFoundedError())
-    post = getPost(postId)
-    if post==None:
-        return sendError(Error.postNotFoundedError())
-    return send (commentService.toDict(commentService.newComment(content , post , user)))
-
-def newUserLike(userId , postId ):
-    user = getUser(userId)
-    if user==None:
-        return sendError(Error.userNotFoundedError())
-    post = getPost(postId)
-    if post==None:
-        return sendError(Error.postNotFoundedError())
-    return send(likeService.toDict(likeService.newLike(post , user)))
-
-def getPost(postId):
-    return postService.getPost(postId)
-
-def getUser(userId):
-    return userService.getUser(userId)
 
 
 
