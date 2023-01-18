e48f064a3fbb172ca3c01badc32c856ca9aa8ad5
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 12:48:59 +0330

    new method for send errors

--- api/main/utils.py
+++ api/main/utils.py
@@ -1,11 +1,15 @@
 from django.http import JsonResponse
 import json
-
+from .errors import Error , newError
 def encode(data):
     return data
 
-def send(data , status):
+def send(data , status=200):
     return JsonResponse(data = encode(data) , safe=False, status=status)
+
+def sendError(err:Error):
+    return JsonResponse(data = err.responseData() , safe=False, status=err.responseStatus())
+
 
 
 def parseBody(b):
