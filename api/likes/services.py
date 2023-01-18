f3a0581c2bacad1b65061c017c339691176182a8
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 12:10:59 +0330

    likes services added

---
+++ api/likes/services.py
@@ -0,0 +1,33 @@
+from .models import Like
+from bson import ObjectId
+
+class LikeService:
+
+    def newLike(self , post , user):
+        like = Like(post = post  , user = user)
+        return self.saveLike(like)
+
+    def getAllLikesByPostId(self , postId):
+        likes = Like.objects.filter(post=ObjectId(postId)).order_by('-created_at').all()
+        return likes
+    
+
+    def saveLike(self , like):
+        like.save()
+        return like
+    
+
+    def toDict(self , like):
+        try:
+            return {
+                "_id":str(like._id),
+                "user_username":like.user.username,
+            }
+        except:
+            return like
+    
+    def toListOfDict(self , likes):
+        return [self.toDict(l) for l in likes]
+
+
+
