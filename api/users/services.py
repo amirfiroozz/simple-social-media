03ec821c413e04a186c60bc54b87eadcb0b0aaae
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 11:36:41 +0330

    user services methods implemented

---
+++ api/users/services.py
@@ -0,0 +1,27 @@
+from .models import User
+from bson import ObjectId
+
+
+class UserServices:
+    def getAllUsers(self):
+        return User.objects.all()
+
+    def getUser(self , userId):
+        return User.objects.filter(_id=ObjectId(userId)).first()
+
+    def newUser(self , username):
+        return self.addUser(User(username = username))
+
+    def addUser(self,user):
+        user.save()
+        return user
+
+    def toDict(self , user:User):
+        return {
+            "_id":str(user._id),
+            "username":user.username,
+        }
+    
+    def toListOfDict(self , users):
+        return [self.toDict(user) for user in users]
+
