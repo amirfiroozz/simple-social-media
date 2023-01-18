0ef80c4c22397d21ed6bd7ea570935a5a72debb0
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 12:29:07 +0330

    comments and likes urls added

--- api/posts/urls.py
+++ api/posts/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('<str:postId>/comments' , postCommentsListView),
+    path('<str:postId>/likes' , postLikesListView),
 ]
