2488fe46792558dc9e7d478fc4a84df96bcc59c9
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2023-01-18 12:28:18 +0330

    comments and likes urls added

--- api/users/urls.py
+++ api/users/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('' , userCreateListView),
     path('<str:id>' , userView),
     path('<str:userId>/posts' , userPostsListCreateView),
+    path('<str:userId>/posts/<str:postId>/comments' , userCommentCreateView),
+    path('<str:userId>/posts/<str:postId>/likes' , userLikeCreateView),
 
 ]
